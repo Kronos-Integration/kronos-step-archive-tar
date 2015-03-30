b0d5a3a44788bdee3975303fa900ad99adaca049
Markus Felten <Markus.Felten@gmx.de>
2015-03-30 09:54:14 +0200

    fixed some typos

--- index.js
+++ index.js
@@ -6,32 +6,35 @@ let tar = require('tar-stream');
 
 exports.createService = function (manager, serviceId, serviceConfig) {
 
-	let extract = tar.extract();
+  let extract = tar.extract();
 
-	let out1 = manager.getEndpoint(serviceId, 'out1');
+  let out1 = manager.getServiceEndpoint(serviceId, 'out1');
 
-	extract.on('entry', function (header, stream, callback) {
+  extract.on('entry', function (header, stream, callback) {
 
-		stream.on('end', function () {
-			callback(); // ready for next entry
-		});
+    stream.on('end', function () {
+      callback(); // ready for next entry
+    });
 
-		try {
-			out1(header, stream, function (err) {
-				if (err) { 
-					console.log(err);
-					stream.resume();
-				}
-			});
-		} catch (e) {
-			console.log(e);
-			stream.resume();
-		}
-	});
+    try {
+      out1(header, stream, function (err) {
+        if (err) { 
+          console.log(err);
+          stream.resume();
+        }
+      });
+    } catch (e) {
+      console.log(e);
+      stream.resume();
+    }
+  });
 
-	let in1 = manager.getEndpoint('in1')();
+  let in1 = manager.getServiceEndpoint(serviceId, 'in1')();
 
-	for (let connection of in1) {
-		connection.stream.pipe(extract);
-	}
+  for (let connection of in1) {
+    connection.stream.pipe(extract);
+  }
+
+  // TODO live service object ?
+  return {};
 };
--- test/index.js
+++ test/index.js
@@ -10,30 +10,30 @@ let path = require('path');
 var assert = require('assert');
 
 describe('tar service declaration', function () {
-	let myManager = manager();
+  let myManager = manager();
 
-	const name = path.join(__dirname,
-		'fixtures/a.tar');
+  const name = path.join(__dirname,
+    'fixtures/a.tar');
 
-	myManager.declareServices({
-		'service1': {
-			'endpoints': {
-				'in1': function* () {
-					yield {
-						info: {
-							name: name
-						},
-						pipe: fs.createReadStream(name)
-					};
-				},
-				'out1': function (info, stream, cb) {
-					console.log(`name: ${info.name}`);
-				}
-			}
-		}
-	});
-	it('declared services should be present', function () {
-		let service = tar.createService(myManager, 'service1', {});
-		assert(service);
-	});
+  myManager.declareServices({
+    'service1': {
+      'endpoints': {
+        'in1': function* () {
+          yield {
+            info: {
+              name: name
+            },
+            stream: fs.createReadStream(name)
+          };
+        },
+        'out1': function (info, stream, cb) {
+          console.log(`name: ${info.name}`);
+        }
+      }
+    }
+  });
+  it('services should be present', function () {
+    let service = tar.createService(myManager, 'service1', {});
+    assert(service);
+  });
 });
