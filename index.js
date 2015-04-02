32432e7c2e0f926a7213f075fda85399842a27a2
Markus Felten <Markus.Felten@gmx.de>
2015-04-02 18:33:16 +0200

    stepImplementation with definition and implementation

--- index.js
+++ index.js
@@ -4,34 +4,54 @@
 
 let tar = require('tar-stream');
 
-exports.initializeStep = function (manager, step) {
+exports.stepImplementation = {
+  "definition": {
+    "endpoints": {
+      "in": {
+        "direction": "in",
+        "mandatory": true
+      },
+      "out": {
+        "direction": "out",
+        "mandatory": true
+      }
+    }
+  },
+  "initialize": function (manager, step) {
+    let extract = tar.extract();
 
-  let extract = tar.extract();
+    /*
+	  let config = step.endpoints.config.implementation();
 
-  let out = step.endpoints.out.implementation();
+	  for (let r of config) {
+	  }
+	*/
 
-  out.next(); // advance to 1st. connection - TODO: needs to be moved into service-manager
+    let out = step.endpoints.out.implementation();
 
-  extract.on('entry', function (header, stream, callback) {
-    stream.on('end', function () {
-      callback(); // ready for next entry
+    out.next(); // advance to 1st. connection - TODO: needs to be moved into service-manager
+
+    extract.on('entry', function (header, stream, callback) {
+      stream.on('end', function () {
+        callback(); // ready for next entry
+      });
+
+      try {
+        out.next({
+          info: header,
+          stream: stream
+        });
+      } catch (e) {
+        console.log(e);
+        stream.resume();
+      }
     });
 
-    try {
-      out.next({
-        info: header,
-        stream: stream
-      });
-    } catch (e) {
-      console.log(e);
-      stream.resume();
+    let in1 = step.endpoints.in.implementation();
+
+    for (let request of in1) {
+      // TODO request.info is ignored here, is someone interested in ?
+      request.stream.pipe(extract);
     }
-  });
-
-  let in1 = step.endpoints.in.implementation();
-
-  for (let request of in1) {
-    // TODO request.info is ignored here is someone interested in ?
-    request.stream.pipe(extract);
   }
 };
--- test/index.js
+++ test/index.js
@@ -10,39 +10,39 @@ let path = require('path');
 var assert = require('assert');
 
 describe('tar service declaration', function () {
-  let myManager = manager();
+	let myManager = manager();
 
-  const name = path.join(__dirname,
-    'fixtures/a.tar');
+	const name = path.join(__dirname,
+		'fixtures/a.tar');
 
-  myManager.declareFlow({
-    "name": "flow1",
-    "steps": {
-      's1': {
-        'endpoints': {
-          'in': function* () {
-            yield {
-              info: {
-                name: name
-              },
-              stream: fs.createReadStream(name)
-            };
-          },
-          'out': function* () {
-            do {
-              let connection =
-                yield;
-              console.log(`name: ${connection.info.name}`);
-              connection.stream.resume();
-            } while (true);
-          }
-        }
-      }
-    }
-  });
-  it('services should be present', function () {
-    let step = myManager.getFlow('flow1').steps.s1;
-    tar.initializeStep(myManager, step);
-    assert(step);
-  });
+	myManager.declareFlow({
+		"name": "flow1",
+		"steps": {
+			's1': {
+				'endpoints': {
+					'in': function* () {
+						yield {
+							info: {
+								name: name
+							},
+							stream: fs.createReadStream(name)
+						};
+					},
+					'out': function* () {
+						do {
+							let connection =
+								yield;
+							console.log(`name: ${connection.info.name}`);
+							connection.stream.resume();
+						} while (true);
+					}
+				}
+			}
+		}
+	});
+	it('services should be present', function () {
+		let step = myManager.getFlow('flow1').steps.s1;
+		tar.stepImplementation.initialize(myManager, step);
+		assert(step);
+	});
 });
