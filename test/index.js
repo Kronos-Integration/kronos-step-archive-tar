08f3ec30a6d557e424a9f9614c81b23535c89a5b
Markus Felten <Markus.Felten@gmx.de>
2015-04-02 22:53:49 +0200

    test for processed content

--- test/index.js
+++ test/index.js
@@ -10,39 +10,48 @@ let path = require('path');
 var assert = require('assert');
 
 describe('tar service declaration', function () {
-	let myManager = manager();
+  let myManager = manager();
 
-	const name = path.join(__dirname,
-		'fixtures/a.tar');
+  const name = path.join(__dirname,
+    'fixtures/a.tar');
 
-	myManager.declareFlow({
-		"name": "flow1",
-		"steps": {
-			's1': {
-				'endpoints': {
-					'in': function* () {
-						yield {
-							info: {
-								name: name
-							},
-							stream: fs.createReadStream(name)
-						};
-					},
-					'out': function* () {
-						do {
-							let connection =
-								yield;
-							console.log(`name: ${connection.info.name}`);
-							connection.stream.resume();
-						} while (true);
-					}
-				}
-			}
-		}
-	});
-	it('services should be present', function () {
-		let step = myManager.getFlow('flow1').steps.s1;
-		tar.stepImplementation.initialize(myManager, step);
-		assert(step);
-	});
+  const names = {};
+
+  myManager.declareFlow({
+    "name": "flow1",
+    "steps": {
+      's1': {
+        'endpoints': {
+          'in': function* () {
+            yield {
+              info: {
+                name: name
+              },
+              stream: fs.createReadStream(name)
+            };
+          },
+          'out': function* () {
+            do {
+              let connection =
+                yield;
+              //console.log(`name: ${connection.info.name}`);
+              names[connection.info.name] = true;
+              connection.stream.resume();
+            } while (true);
+          }
+        }
+      }
+    }
+  });
+
+  it('content should be processed', function (done) {
+    let step = myManager.getFlow('flow1').steps.s1;
+    tar.stepImplementation.initialize(myManager, step);
+
+    // TODO how to know when input is completly processed ?
+    setTimeout(function () {
+      assert(names.file1 && names.file2 && names.file3);
+      done();
+    }, 10);
+  });
 });
