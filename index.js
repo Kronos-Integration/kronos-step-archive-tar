f17ae7f23e4c916a1fb74322ff8a2087da54133d
Markus Felten <Markus.Felten@gmx.de>
2015-04-04 21:50:51 +0200

    registering of steps streamlined

--- index.js
+++ index.js
@@ -4,56 +4,46 @@
 
 let tar = require('tar-stream');
 
-exports.stepImplementation = {
-  "name": "untar",
-  "description": "decomposes tar archive into individual output requests",
-  "endpoints": {
-    "in": {
-      "direction": "in",
-      "uti": "public.tar-archive",
-      "mandatory": true
+exports.stepImplementations = {
+  "untar": {
+    "description": "decomposes tar archive into individual output requests",
+    "endpoints": {
+      "in": {
+        "direction": "in",
+        "uti": "public.tar-archive"
+      },
+      "out": {
+        "direction": "out"
+      }
     },
-    "out": {
-      "direction": "out",
-      "uti": "public.data",
-      "mandatory": true
-    }
-  },
-  "initialize": function (manager, step) {
-    let extract = tar.extract();
+    "initialize": function (manager, step) {
+      const extract = tar.extract();
+      const out = step.endpoints.out.implementation();
 
-    /*
-	  let config = step.endpoints.config.implementation();
+      out.next(); // advance to 1st. connection - TODO: needs to be moved into service-manager
 
-	  for (let r of config) {
-	  }
-	*/
+      extract.on('entry', function (header, stream, callback) {
+        stream.on('end', function () {
+          callback(); // ready for next entry
+        });
 
-    let out = step.endpoints.out.implementation();
-
-    out.next(); // advance to 1st. connection - TODO: needs to be moved into service-manager
-
-    extract.on('entry', function (header, stream, callback) {
-      stream.on('end', function () {
-        callback(); // ready for next entry
+        try {
+          out.next({
+            info: header,
+            stream: stream
+          });
+        } catch (e) {
+          console.log(e);
+          stream.resume();
+        }
       });
 
-      try {
-        out.next({
-          info: header,
-          stream: stream
-        });
-      } catch (e) {
-        console.log(e);
-        stream.resume();
+      const in1 = step.endpoints.in.implementation();
+
+      for (let request of in1) {
+        // TODO request.info is ignored here, is someone interested in ?
+        request.stream.pipe(extract);
       }
-    });
-
-    let in1 = step.endpoints.in.implementation();
-
-    for (let request of in1) {
-      // TODO request.info is ignored here, is someone interested in ?
-      request.stream.pipe(extract);
     }
   }
 };
--- test/index.js
+++ test/index.js
@@ -9,51 +9,52 @@ let path = require('path');
 
 var assert = require('assert');
 
+serviceManger.stepImplementation.register(tar.stepImplementations);
+
 describe('tar service declaration', function () {
-	let myManager = serviceManger.manager();
+  let myManager = serviceManger.manager();
 
-	serviceManger.stepImplementation.registerImplementation(tar.stepImplementation);
-	const name = path.join(__dirname,
-		'fixtures/a.tar');
+  const name = path.join(__dirname,
+    'fixtures/a.tar');
 
-	const names = {};
+  const names = {};
 
-	myManager.declareFlow({
-		"name": "flow1",
-		"steps": {
-			's1': {
-				"type": "untar",
-				"endpoints": {
-					"in": function* () {
-						yield {
-							info: {
-								name: name
-							},
-							stream: fs.createReadStream(name)
-						};
-					},
-					"out": function* () {
-						do {
-							let connection =
-								yield;
-							//console.log(`name: ${connection.info.name}`);
-							names[connection.info.name] = true;
-							connection.stream.resume();
-						} while (true);
-					}
-				}
-			}
-		}
-	});
+  myManager.declareFlow({
+    "name": "flow1",
+    "steps": {
+      's1': {
+        "type": "untar",
+        "endpoints": {
+          "in": function* () {
+            yield {
+              info: {
+                name: name
+              },
+              stream: fs.createReadStream(name)
+            };
+          },
+          "out": function* () {
+            do {
+              let connection =
+                yield;
+              //console.log(`name: ${connection.info.name}`);
+              names[connection.info.name] = true;
+              connection.stream.resume();
+            } while (true);
+          }
+        }
+      }
+    }
+  });
 
-	it('content should be processed', function (done) {
-		let step = myManager.getFlow('flow1').steps.s1;
-		tar.stepImplementation.initialize(myManager, step);
+  it('content should be processed', function (done) {
+    let step = myManager.getFlow('flow1').steps.s1;
+    tar.stepImplementations.untar.initialize(myManager, step);
 
-		// TODO how to know when input is completly processed ?
-		setTimeout(function () {
-			assert(names.file1 && names.file2 && names.file3);
-			done();
-		}, 10);
-	});
+    // TODO how to know when input is completly processed ?
+    setTimeout(function () {
+      assert(names.file1 && names.file2 && names.file3);
+      done();
+    }, 10);
+  });
 });
