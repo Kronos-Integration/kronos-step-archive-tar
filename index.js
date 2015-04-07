593fd9a1ca538924b429637a1474fac79c881d4a
Torsten Link <torsten.link@db.com>
2015-04-07 21:06:11 +0200

    demo

--- index.js
+++ index.js
@@ -5,45 +5,51 @@
 let tar = require('tar-stream');
 
 exports.stepImplementations = {
-  "untar": {
-    "description": "decomposes tar archive into individual output requests",
-    "endpoints": {
-      "in": {
-        "direction": "in",
-        "uti": "public.tar-archive"
-      },
-      "out": {
-        "direction": "out"
-      }
-    },
-    "initialize": function (manager, step) {
-      const extract = tar.extract();
-      const out = step.endpoints.out.implementation();
+	"untar": {
+		"description": "decomposes tar archive into individual output requests",
+		"endpoints": {
+			"in": {
+				"direction": "in",
+				"uti": "public.tar-archive"
+				"info_desc": {
+					"tarFileName": "The file name of the tar ball"
+				}
+			},
+			"out": {
+				"direction": "out"
+				"info_desc": {
+					"detailFileName": "The file name of the extratcted entry"
+				}
+			}
+		},
+		"initialize": function(manager, step) {
+			const extract = tar.extract();
+			const out = step.endpoints.out.implementation();
 
-      out.next(); // advance to 1st. connection - TODO: needs to be moved into service-manager
+			out.next(); // advance to 1st. connection - TODO: needs to be moved into service-manager
 
-      extract.on('entry', function (header, stream, callback) {
-        stream.on('end', function () {
-          callback(); // ready for next entry
-        });
+			extract.on('entry', function(header, stream, callback) {
+				stream.on('end', function() {
+					callback(); // ready for next entry
+				});
 
-        try {
-          out.next({
-            info: header,
-            stream: stream
-          });
-        } catch (e) {
-          console.log(e);
-          stream.resume();
-        }
-      });
+				try {
+					out.next({
+						info: header,
+						stream: stream
+					});
+				} catch (e) {
+					console.log(e);
+					stream.resume();
+				}
+			});
 
-      const in1 = step.endpoints.in.implementation();
+			const in1 = step.endpoints.in.implementation();
 
-      for (let request of in1) {
-        // TODO request.info is ignored here, is someone interested in ?
-        request.stream.pipe(extract);
-      }
-    }
-  }
+			for (let request of in1) {
+				// TODO request.info is ignored here, is someone interested in ?
+				request.stream.pipe(extract);
+			}
+		}
+	}
 };
