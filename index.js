a4158ef74af084db8e70058b4d779c30e5abcb70
Markus Felten <Markus.Felten@gmx.de>
2015-04-08 08:06:02 +0200

    check that archiveName is passed to out endpoint too

--- index.js
+++ index.js
@@ -5,57 +5,64 @@
 let tar = require('tar-stream');
 
 exports.stepImplementations = {
-	"untar": {
-		"description": "decomposes tar archive into individual output requests",
-		"endpoints": {
-			"in": {
-				"direction": "in",
-				"uti": "public.tar-archive",
-				"info_desc": {
-					"tarFileName": {
-						"description": "The file name of the tar ball",
-						"mandatory": false,
-						"default": ","
-					}
-				}
-			},
-			"out": {
-				"direction": "out",
-				"info_desc": {
-					"detailFileName": {
-						"description": "The file name of the extratcted entry"
-					}
-				}
-			}
-		},
-		"initialize": function (manager, step) {
-			const extract = tar.extract();
-			const out = step.endpoints.out.implementation();
+  "untar": {
+    "description": "decomposes tar archive into individual output requests",
+    "endpoints": {
+      "in": {
+        "direction": "in",
+        "uti": "public.tar-archive",
+        "contentInfo": {
+          "name": {
+            "description": "The file name of the original tar file",
+            "mandatory": false
+          }
+        }
+      },
+      "out": {
+        "direction": "out",
+        "contentInfo": {
+          "archiveName": {
+            "description": "The file name of the original tar file"
+          },
+          "name": {
+            "description": "The file name of the extratcted entry"
+          }
+        }
+      }
+    },
+    "initialize": function (manager, step) {
+      const extract = tar.extract();
+      const out = step.endpoints.out.implementation();
 
-			out.next(); // advance to 1st. connection - TODO: needs to be moved into service-manager
+      let archiveName;
 
-			extract.on('entry', function (header, stream, callback) {
-				stream.on('end', function () {
-					callback(); // ready for next entry
-				});
+      out.next(); // advance to 1st. connection - TODO: needs to be moved into service-manager
 
-				try {
-					out.next({
-						info: header,
-						stream: stream
-					});
-				} catch (e) {
-					console.log(e);
-					stream.resume();
-				}
-			});
+      extract.on('entry', function (header, stream, callback) {
+        stream.on('end', function () {
+          callback(); // ready for next entry
+        });
 
-			const in1 = step.endpoints.in.implementation();
+        header.archiveName = archiveName;
 
-			for (let request of in1) {
-				// TODO request.info is ignored here, is someone interested in ?
-				request.stream.pipe(extract);
-			}
-		}
-	}
+        try {
+          out.next({
+            info: header,
+            stream: stream
+          });
+        } catch (e) {
+          console.log(e);
+          stream.resume();
+        }
+      });
+
+      const in1 = step.endpoints.in.implementation();
+
+      for (let request of in1) {
+        archiveName = request.info.name;
+
+        request.stream.pipe(extract);
+      }
+    }
+  }
 };
