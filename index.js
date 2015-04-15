a667c6818992b85f351de4a4435c037be03f735a
Markus Felten <Markus.Felten@gmx.de>
2015-04-15 23:10:19 +0200

    make use of step.error()

--- index.js
+++ index.js
@@ -4,67 +4,66 @@
 const tar = require('tar-stream');
 
 exports.stepImplementations = {
-  "untar": {
-    "description": "decomposes tar archive into individual output requests",
-    "endpoints": {
-      "in": {
-        "direction": "in",
-        "uti": "public.tar-archive",
-        "contentInfo": {
-          "name": {
-            "description": "The file name of the original tar file",
-            "mandatory": false
-          }
-        }
-      },
-      "out": {
-        "direction": "out",
-        "contentInfo": {
-          "archiveName": {
-            "description": "The file name of the original tar file"
-          },
-          "name": {
-            "description": "The file name of the extratcted entry"
-          },
-          "mtime": {
-            "description": "modification time of the entry"
-          }
-        }
-      }
-    },
-    "initialize": function (manager, step) {
-      const extract = tar.extract();
-      const out = step.endpoints.out.implementation();
+	"untar": {
+		"description": "decomposes tar archive into individual output requests",
+		"endpoints": {
+			"in": {
+				"direction": "in",
+				"uti": "public.tar-archive",
+				"contentInfo": {
+					"name": {
+						"description": "The file name of the original tar file",
+						"mandatory": false
+					}
+				}
+			},
+			"out": {
+				"direction": "out",
+				"contentInfo": {
+					"archiveName": {
+						"description": "The file name of the original tar file"
+					},
+					"name": {
+						"description": "The file name of the extratcted entry"
+					},
+					"mtime": {
+						"description": "modification time of the entry"
+					}
+				}
+			}
+		},
+		"initialize": function (manager, step) {
+			const extract = tar.extract();
+			const out = step.endpoints.out.implementation();
 
-      let archiveName;
+			let archiveName;
 
-      out.next(); // advance to 1st. connection - TODO: needs to be moved into service-manager
+			out.next(); // advance to 1st. connection - TODO: needs to be moved into service-manager
 
-      extract.on('entry', function (header, stream, callback) {
-        stream.on('end', function () {
-          callback(); // ready for next entry
-        });
+			extract.on('entry', function (header, stream, callback) {
+				stream.on('end', function () {
+					callback(); // ready for next entry
+				});
 
-        header.archiveName = archiveName;
+				header.archiveName = archiveName;
 
-        try {
-          out.next({
-            info: header,
-            stream: stream
-          });
-        } catch (e) {
-          console.log(e);
-          stream.resume();
-        }
-      });
+				try {
+					out.next({
+						info: header,
+						stream: stream
+					});
+				} catch (e) {
+					step.error(e);
+					stream.resume();
+				}
+			});
 
-      const in1 = step.endpoints.in.implementation();
+			const in1 = step.endpoints.in.implementation();
 
-      for (let request of in1) {
-        archiveName = request.info.name;
-
-        request.stream.pipe(extract);
-      }
-    }
-  }
+			for (let request of in1) {
+				archiveName = request.info.name;
+				request.stream.pipe(extract);
+			}
+		}
+	}
 };
