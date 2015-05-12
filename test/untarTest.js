d1516fcec42daa1735048ad3d72598c894cf35b7
Markus Felten <Markus.Felten@gmx.de>
2015-05-12 15:16:08 +0200

    make test work again (kronos_untar) use step logging

--- test/untarTest.js
+++ test/untarTest.js
@@ -11,58 +11,58 @@ const assert = require('assert');
 
 describe('untar service declaration', function () {
 
-  const name = path.join(__dirname,
-    'fixtures/a.tar');
+	const name = path.join(__dirname,
+		'fixtures/a.tar');
 
-  const names = {};
-  let archiveName;
+	const names = {};
+	let archiveName;
 
-  const flowDecls = {
-    "flow1": {
-      "steps": {
-        's1': {
-          "type": "untar",
-          "endpoints": {
-            "in": function* () {
-              yield {
-                info: {
-                  name: name
-                },
-                stream: fs.createReadStream(name)
-              };
-            },
-            "out": function* () {
-              do {
-                let connection =
-                  yield;
-                //console.log(`name: ${connection.info.name}`);
-                names[connection.info.name] = true;
-                archiveName = connection.info.archiveName;
+	const flowDecls = {
+		"flow1": {
+			"steps": {
+				's1': {
+					"type": "kronos_untar",
+					"endpoints": {
+						"in": function* () {
+							yield {
+								info: {
+									name: name
+								},
+								stream: fs.createReadStream(name)
+							};
+						},
+						"out": function* () {
+							do {
+								let connection =
+									yield;
+								//console.log(`name: ${connection.info.name}`);
+								names[connection.info.name] = true;
+								archiveName = connection.info.archiveName;
 
-                connection.stream.resume();
-              } while (true);
-            }
-          }
-        }
-      }
-    }
-  };
+								connection.stream.resume();
+							} while (true);
+						}
+					}
+				}
+			}
+		}
+	};
 
-  it('content should be processed', function (done) {
-    const myManager = kronos.manager({
-      flows: flowDecls,
-      stepDirectories: path.join(__dirname, '..', 'lib')
-    }).then(function (manager) {
-      const flow1 = manager.flowDefinitions.flow1;
-      const step = flow1.steps.s1;
-      flow1.initialize(manager);
+	it('content should be processed', function (done) {
+		const myManager = kronos.manager({
+			flows: flowDecls,
+			stepDirectories: path.join(__dirname, '..', 'lib')
+		}).then(function (manager) {
+			const flow1 = manager.flowDefinitions.flow1;
+			const step = flow1.steps.s1;
+			flow1.initialize(manager);
 
-      // TODO how to know when input is completly processed ?
-      setTimeout(function () {
-        assert(names.file1 && names.file2 && names.file3);
-        assert(archiveName === name);
-        done();
-      }, 10);
-    });
-  });
+			// TODO how to know when input is completly processed ?
+			setTimeout(function () {
+				assert(names.file1 && names.file2 && names.file3);
+				assert(archiveName === name);
+				done();
+			}, 10);
+		});
+	});
 });
