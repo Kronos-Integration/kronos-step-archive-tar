e20abe100ab8895a25c990fb76c458115e466105
Torsten Link <torsten.link@db.com>
2015-07-28 21:23:21 +0200

    untar

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-exports.registerWithManager = function(manager) {
-  manager.registerStepImplementation('kronos_untar',require('./lib/untar'));
+/* jslint node: true, esnext: true */
+"use strict";
+exports.registerWithManager = function (manager) {
+  manager.registerStepImplementation('kronos_untar', require('./lib/untar'));
 };
