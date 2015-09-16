16af15206eeb348c138a9d44640c91eff34a80fb
Markus Felten <Markus.Felten@gmx.de>
2015-09-16 22:39:11 +0200

    refactor: step defintion now holds the definitions name

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /* jslint node: true, esnext: true */
 "use strict";
 exports.registerWithManager = function (manager) {
-  manager.registerStepImplementation('kronos-untar', require('./lib/untar'));
-  manager.registerStepImplementation('kronos-tar', require('./lib/tar'));
+  manager.registerStepImplementation(require('./lib/untar'));
+  manager.registerStepImplementation(require('./lib/tar'));
 };
